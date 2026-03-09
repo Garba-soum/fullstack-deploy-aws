a46a53592ce14a65c97db910ff61f89b842b0efa
Soumana Ibrahim <ibrahim_soumana1@yahoo.com>
2026-03-09 21:26:54 +0100

    fix api url for aws deployment

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 
-    apiUrl: 'http://localhost:8080' // URL de base du backend Spring Boot
+    apiUrl: 'http://13.39.13.100:8081' // URL de base du backend Spring Boot
 };
